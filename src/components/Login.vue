69c7f11346b433c85215658a6388c05bd2e7a2c6
lyh <1065088060@qq.com>
2020-12-01 19:01:27 +0800

    elment-ui

---
+++ src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <el-form label-width="0px">
+                <el-form-item >
+                    <el-input></el-input>
+                </el-form-item><br><br><br>
+                <el-form-item >
+                    <el-input></el-input>
+                </el-form-item>
+                <el-button type="primary">主要按钮</el-button>
+                <el-button type="primary">主要按钮</el-button>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+.avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #fff;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #eee;
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '../plugin/element'
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
